ded4d1a67af88e2fbf6619e83d5aa12247e64967
Nikita Plotnikov <plotnikov_n@auca.kg>
2026-05-13 17:01:32 +0700

    PWA compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './app/App';
-import './styles/global.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./app/App";
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        const swUrl = new URL("sw.js", document.baseURI).toString();
+
+        navigator.serviceWorker.register(swUrl).catch((error) => {
+            console.error("Service worker registration failed:", error);
+        });
+    });
+}
